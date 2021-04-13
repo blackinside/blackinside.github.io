9137774efdc5d19956f226f08050b8c1b8af725e
WarrenCoin <82334206+Inside-global@users.noreply.github.com>
2021-04-13 14:23:30 -0300

    sitio web
    sitio web

---
+++ assets/js/charts-pie.js
@@ -0,0 +1,35 @@
+/**
+ * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
+ */
+const pieConfig = {
+  type: 'doughnut',
+  data: {
+    datasets: [
+      {
+        data: [5 , 5 , 10 , 80 ],
+        /**
+         * These colors come from Tailwind CSS palette
+         * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+         */
+        backgroundColor: ['#0694a2', '#1c64f2', '#fb0000', '#9e3af2', '#323af2', ],
+        label: 'Dataset 1',
+      },
+    ],
+    labels: ['Airdrops 21.000', 'Team 21.000', 'Private Sale 42.000', 'Honeyswap 336.000'],
+  },
+  options: {
+    responsive: true,
+    cutoutPercentage: 80,
+    /**
+     * Default legends are ugly and impossible to style.
+     * See examples in charts.html to add your own legends
+     *  */
+    legend: {
+      display: false,
+    },
+  },
+}
+
+// change this to the id of your chart element in HMTL
+const pieCtx = document.getElementById('pie')
+window.myPie = new Chart(pieCtx, pieConfig)
